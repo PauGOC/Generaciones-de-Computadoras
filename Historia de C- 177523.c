bb951dc1fab997755be77de87a4e03d84fd9b7a9
PauGOC <141955969+PauGOC@users.noreply.github.com>
2023-08-15 18:20:34 -0600

    Add files via upload

---
+++ Historia de C- 177523.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main (int argc, char ** argv)
+{
+	setlocale(LC_ALL,"");
+	system("cls");
+	printf("         \n");
+	printf("\033[0;100m");
+	printf("\t\t\t\t\t HISTORIA DE LAS COMPUTADORAS \n");
+	printf("\033[0;90m");
+	printf("              \n");
+	printf("\033[1;37m");
+	printf("\t\t\t\t\tPaulina Gómez Olvera C. -177523");
+	printf("              \n");
+	printf("     \n        \n");
+	printf("\033[0;31m");
+	printf("Primera Generación (1940-1950):\n");
+	printf("\033[1;37m");
+	printf("Las computadoras electrónicas comenzaron a desarrollarse. La ENIAC \n (Electronic Numerical Integrator and Computer), construida en 1945, \n era ua máquina enorme que utilizaba más de 17;000 válvulas de vacío para realizar \n cálculos complejos. El diseño y desarrollo de la ENIAC estuvo a cargo \n de J. Presper Eckert y John Mauchly.");
+	printf("              \n");
+	printf("              \n");
+	printf("\033[1;34m");
+	printf("Segunda Generación (1950-1960): \n");
+	printf("\033[1;37m");
+	printf("En esta etapa, se introdujeron las válvulas de vacío por transistores \n, lo que permitió reducir el tamaño de las computadoras. También,\n la programación se volvió más práctica gracias al desarrollo del \n lenguage Fortran.");
+	printf("               \n");
+	printf("               \n");
+	printf("\033[0;36m");
+	printf("Tercera Generación (1960-1970): \n");
+	printf("\033[1;37m");
+	printf("Los circuitos integrados (chips) comenzaron a utilizarse gracias a Jack\n Kilby y Robert Noyce. En 1964, IBM lanzó la serie Systems-360 que ofrecía \n compatibilidad entre modelos y tamaños. Además, el lenguaje de programación \n COBOL se convirtió en una herramienta para el desarrollo de software empresarial. ");
+	printf("     \n");
+	printf("       \n");
+	printf("\033[0;33m");
+	printf("Cuarta Generación (1970-1980): \n");
+	printf("\033[1;37m");
+	printf("Los microprocesadores surgieron , lo que permitió que toda la CPU se \n colocara en un solo chip. Además de que en esta época se crearon las \n primeras computadoras personales tal y como lo fue la Altair 8800 y \n Apple II. También se crearon lenguajes de programación de alto nivel, como C. ");
+	printf("   \n");
+	printf("     \n");
+	printf("\033[1;32m");
+	printf("Quinta Generación (1980-1990): \n");
+	printf("\033[1;37m");
+	printf("En esta década, los avances tecnológicos relacionados a los \n semiconductores mejoraron la velocidad y potencia de las computadoras.\n Asimismo, Apple presentola Macintosh con interfaz gráica de usuario.\n Richard Stallman y Linus Torvalds contribuyeron con el desarrollo del\n software de código abierto y el sistema operativo Linux.");
+	printf("    \n");
+	printf("    \n");
+	printf("\033[1;36m");
+	printf("Sexta Generación (1990-2000) \n");
+	printf("\033[1;37m");
+	printf("Durante esta década, se creo el internet y la World Wide Web lo que \n cambió de manera radical la forma en la que los usuarios interactuaban \n con las computadoras y revolucionó la comunicación y el acceso a \n la información.");
+	printf(" \n");
+	printf("     \n");
+	printf("\033[1;31m");
+	printf("Séptima Generación (2000-2010): \n");
+	printf("\033[1;37m");
+	printf("La potencia de procesamiento continuó en aumento, mientas, la mobilidad\n se convirtió en una característica para los dispositivos tecnológicos, \n tal como los teléfonos móbiles y las tabletas. De la misma forma, se \n logró un mejor almacenamiento de datos y el acceso a ellos\n gracias a la computación de la nube.");
+	printf("    \n");
+	printf("    \n");
+	printf("\033[1;33m");
+	printf("Octava Generación (2010- presente): \n");
+	printf("\033[1;37m");
+	printf("En la actualidad, el enfoque de la computación se encuentra en la eficiencia \n energética, la inteligencia artificial, la realidad virtual y la \n computación cuántica con sus respectivos campos de aplicación como la \n tecnología, la ciencia, en el entretenimiento, entre otras. ");
+	printf("\n\n\n\n");
+	return 0;
+}
